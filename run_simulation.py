23b6506e4a9a2f924b31e10cc291b4464129096c
kundroomajid <kundroomajid@gmail.com>
2023-09-16 17:29:57 +0900

    adds feature to run simulation in multi-node environment

--- run_simulation.py
+++ run_simulation.py
@@ -1,4 +1,5 @@
 import os
+import mak
 from mak.utils import get_strategy, generate_config_simulation, get_eval_fn, gen_dir_outfile_server
 from mak.utils import save_simulation_history
 from mak.utils import create_model, compile_model,get_eval_fn, fit_config
@@ -6,6 +7,7 @@ from mak.utils import create_model, compile_model,get_eval_fn, fit_config
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
 import flwr as fl
+from flwr.simulation.ray_transport.utils import enable_tf_gpu_growth
 from client import generate_client
 from mak.custom_server import ServerSaveData
 
@@ -13,6 +15,22 @@ from mak.custom_server import ServerSaveData
 def main() -> None:
     # Start Flower simulation
     config = generate_config_simulation(c_id=-1) # c_id = -1 implies the confugration is for server
+    if config['gpu']:
+        enable_tf_gpu_growth()
+        actor_kwargs = {"on_actor_init_fn" : enable_tf_gpu_growth}
+    else:
+        actor_kwargs = {}
+
+    if config['num_cpus'] and (config['num_gpus'] > 0.0 and config['num_gpus'] <= 1.0):
+        client_res = {'num_cpus': config['num_cpus'], 'num_gpus' : config['num_gpus']}
+    else:
+        client_res = {'num_cpus': config['num_cpus'], 'num_gpus' : 0.0}
+    
+    if config['multi_node']:
+        ray_init_args = {"address" : "auto","runtime_env" : {"py_modules" : [mak]}} #if multi-node cluster is used
+    else:
+        ray_init_args = {}
+
     dataset = config['dataset']
     num_clients = config['min_avalaible_clients']
     if dataset == 'cifar-10':
@@ -33,16 +51,18 @@ def main() -> None:
     strategy = get_strategy(config=config,get_eval_fn=get_eval_fn,model=model,dataset=dataset,num_clients=num_clients,on_fit_config_fn=fit_config)
     server = ServerSaveData(
         strategy=strategy, client_manager=fl.server.client_manager.SimpleClientManager(),out_file_path=out_file_path,target_acc=config['target_acc'])
-    
+   
     hist = fl.simulation.start_simulation(
         client_fn=generate_client,
         num_clients=config['min_avalaible_clients'],
         config=fl.server.ServerConfig(num_rounds=config['max_rounds']),
         strategy = strategy,
         server = server,
+        client_resources = client_res,
+        actor_kwargs = actor_kwargs,
+        ray_init_args = ray_init_args,
     )
-    
-   
+
     simu_data_file_path = out_file_path.replace('.csv','_metrics.csv')
     save_simulation_history(hist=hist,path = simu_data_file_path)
 
